e5c3f009bcd0ad65746afc2d2d7fe9902affb0a4
Lucas <18381968+lucaspunz@users.noreply.github.com>
2023-11-29 12:42:38 -0800

    adjust styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import {EmailSpamClassifier} from '../components/email-spam-classifier'
+import Image from "next/image";
+import { EmailSpamClassifier } from "../components/email-spam-classifier";
 
 export default function Home() {
   return (
     <main>
-      <EmailSpamClassifier/>
+      <EmailSpamClassifier />
     </main>
-  )
+  );
 }
--- frontend/src/components/email-spam-classifier.tsx
+++ frontend/src/components/email-spam-classifier.tsx
@@ -1,46 +1,54 @@
 "use client";
-import { Button } from "@/components/ui/button"
-import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card"
-import { useState } from "react"
+import { Button } from "@/components/ui/button";
+import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
+import { useState } from "react";
 
 export function EmailSpamClassifier() {
-  const [emailContent, setEmailContent] = useState<string>("")
+  const [emailContent, setEmailContent] = useState<string>("");
 
-  const [results, setResults] = useState<{logistic: number}>({logistic: 0})
+  const [results, setResults] = useState<{ logistic: number }>({ logistic: 0 });
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+    event.preventDefault();
 
     const response = await fetch("http://localhost:5000/predict/logistic", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ input:emailContent }),
-    })
-    const data = await response.json()
+      body: JSON.stringify({ input: emailContent }),
+    });
+    const data = await response.json();
     setResults({
-      logistic: data.logistic
-    })
+      logistic: data.logistic,
+    });
   }
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-      <div className="container px-4 md:px-6">
+      <div className="px-4 md:px-6">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
+            {/* write a header saying this is for ECS 171 Group 5 */}
+            <h2 className="text-xl font-semibold tracking-tighter sm:text-2xl text-zinc-500 dark:text-zinc-400">
+              ECS 171 Fall 2023 Group 5 - Joe, Zhenshuo, Amar, Lucas
+            </h2>
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
               Email Spam Classifier
             </h1>
-            <p className="mx-auto max-w-[700px] text-zinc-500 md:text-xl dark:text-zinc-400">
-              Enter the content of your email and our machine learning models will predict if it's spam or not.
+            <p className="mx-auto max-w-[700px] text-zinc-400 md:text-lg dark:text-zinc-500">
+              Enter the content of your email and our machine learning models
+              will predict if it's spam or not.
             </p>
           </div>
           <div className="w-full space-y-2">
-            <form className="flex flex-col space-y-2" onSubmit={handleSubmit}>
+            <form
+              className="flex flex-col space-y-2 items-center"
+              onSubmit={handleSubmit}
+            >
               <textarea
                 aria-label="Email content"
-                className="max-w-lg flex-1 h-56 p-2 border border-zinc-600/50 dark:border-zinc-500 dark:bg-zinc-700/50 rounded-md text-black dark:text-white"
-                placeholder="Enter your email content"
+                className="w-[80vw] p-2 border border-zinc-600/50 dark:border-zinc-500 dark:bg-zinc-700/50 rounded-md text-black dark:text-white"
+                placeholder="Email"
                 value={emailContent}
                 onChange={(event) => setEmailContent(event.target.value)}
               />
@@ -61,11 +69,11 @@ export function EmailSpamClassifier() {
               <CardTitle>Logistic</CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Spam Probability: {results.logistic*100}%</p>
+              <p>Spam Probability: {results.logistic * 100}%</p>
             </CardContent>
           </Card>
         </div>
       </div>
     </section>
-  )
+  );
 }
